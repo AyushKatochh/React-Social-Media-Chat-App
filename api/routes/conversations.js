9ef404a88915571731f6e0d0e49a338fa9f6c0f5
Ayush Katoch <katochayushhp@gmail.com>
2022-05-03 23:49:58 +0530

    Update conversations.js

--- api/routes/conversations.js
+++ api/routes/conversations.js
@@ -17,7 +17,16 @@ router.post("/", async (req, res) => {
     }
 })
 //get conversation of a user
-
+router.get("/:userId", async(req, res) => {
+    try{
+       const conversation = await Conversation.find({
+           members: {$in: [req.params.userId]},
+       });
+       res.status(200).json(conversation)
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
 
 
 module.exports = router;
