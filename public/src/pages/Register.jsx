42dc90841f31ccc1865b18cf059c9e7306c7a217
Ayush Katoch <katochayushhp@gmail.com>
2022-05-22 23:42:39 +0530

    Update Register.jsx

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -4,6 +4,7 @@ import Logo from "../assets/logo.svg";
 import {Link} from "react-router-dom";
 import {ToastContainer, toast} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 export default function Register() {
     const [values, setValues] = useState({
@@ -21,21 +22,40 @@ export default function Register() {
                 theme: "dark",
          
     }
-    const handleSubmit = (event) => {
+    const handleSubmit = async(event) => {
         event.preventDefault();
-        handleValidation();
+      if ( handleValidation()) {
+           const {password, confirmPassword, username, email} = values;
+           const {data} = await axios.post()
+      }
     }
 
     const handleValidation = () => {
         const {password, confirmPassword, username, email} = values;
         if(password !== confirmPassword) {
-            console.log("inm validation", toast);
+        
             // Using react-toastify here
-            toast.error("Password and Confirm Password are same", {toastOptions
-                  });
+            toast.error("Password and Confirm Password are not same", toastOptions
+                  );
+                  return false;
+
            
+        } else if(username.length < 3) {
+            toast.error("Username should be greater than 3 characters", toastOptions);
+          return false;
+       
+        } else if(password.length < 8) {
+            toast.error("Password should be equal or greater than 8 characters", toastOptions);
+          return false;
+        
+        } else if(email==="") {
+            toast.error("Email is required", toastOptions);
+            return false;
         }
+        return true;
+           
     }
+
  
     const handleChange = (event) => {
         setValues({...values, [event.target.name]: event.target.value});
