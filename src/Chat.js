63876cf085c9a677221a05e974b3cc76714ae5f7
Ayush Katoch <katochayushhp@gmail.com>
2022-06-01 23:32:55 +0530

    Update Chat.js

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,34 @@
+import { Avatar, IconButton } from '@mui/material';
 import React from 'react'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
 
 function Chat() {
   return (
     <div className='chat'>
-        <h1>Chat</h1>
+      <div className='chat__header'>
+        <Avatar />
+
+        <div className='chat__headerInfo'>
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
         </div>
+
+        <div className='chat__headerRight'>
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+            
+          <IconButton>
+               <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+              </div>
   )
 }
 
