1248a404a88067c8ad6d0d1614fce9ce145a3ef9
Ayush Katoch <katochayushhp@gmail.com>
2022-06-04 23:58:45 +0530

    Update Chat.js

--- src/Chat.js
+++ src/Chat.js
@@ -1,8 +1,11 @@
+import "./Chat.css";
 import { Avatar, IconButton } from '@mui/material';
 import React from 'react'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import SearchIcon from '@mui/icons-material/Search';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import MicIcon from '@mui/icons-material/Mic';
 
 function Chat() {
   return (
@@ -28,6 +31,39 @@ function Chat() {
           </IconButton>
         </div>
       </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Ayush</span>
+          This is a message
+
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}2
+          </span>
+          </p>
+
+         <p className="chat__message chat__receiver">
+          <span className="chat__name">Ayush</span>
+          This is a message
+
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}2
+          </span>
+          </p>
+
+            </div>
+         
+         <div className="chat__footer">
+             <InsertEmoticonIcon />
+             <form>
+               <input placeholder="Type a message" type="text" />
+               <button  type="submit">
+                 Send a message
+               </button>
+             </form>
+             <MicIcon />
+         </div>
+            
               </div>
   )
 }
